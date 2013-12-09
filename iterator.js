8379865156e414769022d1765bdd4dfca36d8243
Deborah Maaijen <dmaaijen@insign.it>
2013-12-09 17:11:14 +0100

    added http get function to read a JSON file

--- iterator.js
+++ iterator.js
@@ -1,33 +1,41 @@
-function listIter($scope) {
-  $scope.foods = [
-    {text: 'Apple'},
-    {text: 'Cookie'},
-    {text: 'Sandwich'},
-    {text: 'Pastry'},
-    {text: 'Snack-a-Jacks'}			
-  ];
 
-  $scope.addFood = function(){
-    $scope.foods.push({text: $scope.food});
-    $scope.food = "";
-  }
-}
-
-/*var foodApp = angular.module('foodApp', []);
+var foodApp = angular.module('foodApp', []);
 
   foodApp.controller('foodControl', ['$scope', function($scope){
 
-  $scope.foods = [
-    {text: 'Apple'},
-    {text: 'Cookie'},
-    {text: 'Sandwich'},
-    {text: 'Pastry'},
-    {text: 'Snack-a-Jacks'}			
-  ];
+		  $scope.foods = [
+			{text: 'Apple'},
+			{text: 'Cookie'},
+			{text: 'Sandwich'},
+			{text: 'Pastry'},
+			{text: 'Snack-a-Jacks'}			
+		  ];
 
-  $scope.addFood = function(){
-    $scope.foods.push({text: $scope.food});
-    $scope.food = "";
+		  $scope.addFood = function(){
+			$scope.foods.push({text: $scope.food});
+			$scope.food = "";
+		  }  
+  }]);
+
+  foodApp.controller('labelControl', ['$scope','$http', 
+    function($scope, $http) {
+      
+      $scope.labels = [];
+      $http.get('http://api.trello.com/1/boards/4d5ea62fd76aa1136000000c').success(function(data) { 
+        for(var i = 0; i < data.length; i++ ) {
+            $scope.labels.push(data.labelNames[i]);
+        } 
+  	  });	  
+    }
+  ]);
+
+foodApp.directive('osListcopy', function() {
+  return {
+    restrict: 'AE',
+    scope: { 
+       foods :'='
+    },
+    template: '<ul><li ng-repeat="fooditem in foods | filter:search" >{{fooditem.text }}</li></ul>'
   }
-}]);
-*/
+});
+
